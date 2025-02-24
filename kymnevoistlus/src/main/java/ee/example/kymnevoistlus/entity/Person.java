2d5b078a2908eec1c8d5822619bc3c501903851d
Diana Vladotsenko <diana.vladotsenko@gmail.com>
2025-02-24 21:31:40 +0200

    added event in person

--- kymnevoistlus/src/main/java/ee/example/kymnevoistlus/entity/Person.java
+++ kymnevoistlus/src/main/java/ee/example/kymnevoistlus/entity/Person.java
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:ae6dc81195c856f85f0bb2173dd2d8eb479085d82d39e04cc21ab337448cf22e
-size 581
+oid sha256:7ebcea37025ceb81bf98e10b67ce629943bdbfd0ba9750f250c4af018e8ef433
+size 664
