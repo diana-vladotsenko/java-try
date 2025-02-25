d1ba8215d93d2d7058e22ad813a73823687a1982
Diana Vladotsenko <diana.vladotsenko@gmail.com>
2025-02-25 14:02:42 +0200

    added exception

---
+++ kymnevoistlus/src/main/java/ee/example/kymnevoistlus/exception/ErrorMessage.java
@@ -0,0 +1,3 @@
+version https://git-lfs.github.com/spec/v1
+oid sha256:2329023005338cc5f83c0302d15cd130a1021f3b6279378c8d2f31b0d93ae0f0
+size 221
---
+++ kymnevoistlus/src/main/java/ee/example/kymnevoistlus/exception/ExceptionCatcher.java
@@ -0,0 +1,3 @@
+version https://git-lfs.github.com/spec/v1
+oid sha256:f88b8bd8d537e729dedd9df3fff6cc131a080db2b92bcda5d6df7a0fef2020b7
+size 653
