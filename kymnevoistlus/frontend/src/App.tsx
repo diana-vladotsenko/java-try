e1353cb059c7d63df73f42fd3ed28462b9227923
Diana Vladotsenko <diana.vladotsenko@gmail.com>
2025-03-17 19:40:50 +0200

    added objects in frontend

---
+++ kymnevoistlus/frontend/src/App.tsx
@@ -0,0 +1,16 @@
+import './App.css'
+
+function App() {
+  //const [count, setCount] = useState(0)
+
+  return (
+    <>
+    <h1>People:</h1>
+
+
+    <h1>Events:</h1>
+    </>
+  )
+}
+
+export default App
